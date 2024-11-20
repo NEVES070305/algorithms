695bafa169ee14354a185f0daf3adfe37c802b52
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-11-20 20:22:22 -0300

    feat(MergeSort): added merge sort

---
+++ MergeSort/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 38, 27, 43, 3, 9, 82, 10 };
+
+            Console.WriteLine("Array original:");
+            Console.WriteLine(string.Join(", ", array));
+
+            MergeSort.MergeSortFunction(array);
+
+            Console.WriteLine("Array ordenado:");
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
