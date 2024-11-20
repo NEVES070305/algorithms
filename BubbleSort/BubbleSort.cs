3984818b169830183a91afdea15b726b210f1663
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-11-20 19:21:47 -0300

    feat(BubbleSort): added sorting method bubble sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class BubbleSort
+    {
+        public static List<int> BubbleSortFunction(List<int> list) 
+        {
+            for (int i = 0; i < list.Count; i++) 
+            {
+                bool swapped = false;
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int key = list[j];
+                        list[j] = list[j+1];
+                        list[j+1] = key;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+            return list;
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Escreva um número: ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int number))
+                {
+                    list.Add(number);
+                    BubbleSort.BubbleSortFunction(list);
+                    Console.WriteLine("Lista ordenada: " + string.Join(",", list));
+                }
+                else
+                {
+                    Console.WriteLine("Digite corretamente.");
+                }
+            }
+        }
+    }
+}
